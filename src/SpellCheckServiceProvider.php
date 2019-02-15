24a5b8818a98e0e3e565365e373f44ec6c31490f
Robin Sharma <rsharma@torinit.com>
2019-02-15 19:06:48 +0530

    Initial Commit

---
+++ src/SpellCheckServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Robin\SpellCheker;
+use Illuminate\Support\ServiceProvider;
+
+class SpellChekerServiceProvider extends ServiceProvider {
+    public function boot()
+    {
+        //
+    }
+    public function register()
+    {
+        //
+    }
+}
+?>
